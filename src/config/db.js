8cc539d002ff5ba04f2d6085304ec2c68b7a9f52
abdmmar <opowae909@gmail.com>
2021-07-09 14:48:24 +0700

    Add mongodb atlas and user admin after initialized database

--- src/config/db.js
+++ src/config/db.js
@@ -1,28 +1,64 @@
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
+import User from '../model/User.model.js'
 
 dotenv.config()
 
-const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOSTNAME, MONGO_PORT, MONGO_DB } =
-  process.env
+const {
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+  MONGO_HOSTNAME,
+  MONGO_HOSTNAME_ATLAS,
+  MONGO_PORT,
+  MONGO_DB,
+} = process.env
 
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  // reconnectTries: Number.MAX_VALUE,
-  // reconnectInterval: 1000,
   connectTimeoutMS: 10000,
 }
 
+// Use with docker
 const URL = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`
+
+//Use with MongoDB Atlas
+const MONGO_URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME_ATLAS}/${MONGO_DB}?retryWrites=true&w=majority`
+
 mongoose
-  .connect(URL, options)
-  .then(() => {
+  .connect(MONGO_URI, options)
+  .then(async () => {
     console.log('Database is connected')
+
+    const isAdminExists = await User.exists({
+      email: 'admin@mail.com',
+      username: 'admin',
+      role: 'admin',
+    })
+
+    if (isAdminExists) {
+      console.log('User admin already created')
+      return
+    }
+
+    const admin = await User.create({
+      first_name: 'admin',
+      last_name: 'tamvan',
+      email: 'admin@mail.com',
+      age: 21,
+      username: 'admin',
+      password: 'admin123',
+      role: 'admin',
+    })
+
+    if (admin) {
+      console.log('User admin created successfully')
+    } else {
+      console.log('User admin failed to create')
+    }
   })
   .catch((err) => {
     console.error(err)
   })
-// mongoose.Promise = global.Promise
 
 export default mongoose
